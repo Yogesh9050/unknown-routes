a6bad235d46a255be6eab163e3ac1eb7c057ff69
Yogesh9050 <yogesh.sh9050@gmail.com>
2025-02-25 18:29:06 +0530

    logging middleware
    Added middleware to log the HTTP method and endpoint accessed

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-// Middleware to parse JSON request bodies
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next(); // Pass control to the next middleware or route handler
+});
+
 app.use(express.json());
 
-// Routes
+
 app.get('/products', (req, res) => {
     res.send('Here is the list of all products.');
 });
@@ -23,12 +28,10 @@ app.post('/categories', (req, res) => {
     res.send('A new category has been created.');
 });
 
-// Wildcard route for undefined endpoints
 app.use('*', (req, res) => {
     res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
 });
